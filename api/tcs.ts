e89b09b98a906211020c0f9b844f454ed7d63f49
Bikram Rongpi <bikramrongpi088@gmail.com>
2024-02-19 17:31:54 +0530

    agr

--- api/tcs.ts
+++ api/tcs.ts
@@ -1,4 +1,5 @@
-// import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+const fs = require('fs');
 
 // export default function handler(req: VercelRequest, res: VercelResponse) {
 //   const { name = 'walal' } = req.query
@@ -8,29 +9,42 @@
 //           }
 
 
+// Define the handler function
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  // Read the HTML file from the filesystem
+    fs.readFile('/path/to/your/html/file.html', 'utf8', (err, data) => {
+        if (err) {
+              // Handle error if the file cannot be read
+                    res.status(500).json({ error: 'Internal Server Error' });
+                        } else {
+                              // Send the HTML content as a response
+                                    res.status(200).send(data);
+                                        }
+                                          });
+                                          }
 
-// server.js
-const { createServer } = require('@vercel/node');
-const fs = require('fs');
+                                          
 
-const server = createServer((req, res) => {
-  if (req.url === '/') {
-      // Serve the index.html file
-          fs.readFile(__dirname + '/index.html', (err, data) => {
-                if (err) {
-                        res.writeHead(500, { 'Content-Type': 'text/plain' });
-                                res.end('Internal Server Error');
-                                      } else {
-                                              res.writeHead(200, { 'Content-Type': 'text/html' });
-                                                      res.end(data);
-                                                            }
-                                                                });
-                                                                  } else {
-                                                                      // Handle other requests
-                                                                          res.writeHead(404, { 'Content-Type': 'text/plain' });
-                                                                              res.end('Not Found');
-                                                                                }
-                                                                                });
+// // server.js
+// const { createServer } = require('@vercel/node');
 
-                                                                                module.exports = server;
-                                                                                
+// const server = createServer((req, res) => {
+//   if (req.url === '/') {
+//       // Serve the index.html file
+//           fs.readFile(__dirname + '/index.html', (err, data) => {
+//                 if (err) {
+//                         res.writeHead(500, { 'Content-Type': 'text/plain' });
+//                                 res.end('Internal Server Error');
+//                                       } else {
+//                                               res.writeHead(200, { 'Content-Type': 'text/html' });
+//                                                       res.end(data);
+//                                                             }
+//                                                                 });
+//                                                                   } else {
+//                                                                       // Handle other requests
+//                                                                           res.writeHead(404, { 'Content-Type': 'text/plain' });
+//                                                                               res.end('Not Found');
+//                                                                                 }
+//                                                                                 });
+
+//                                                                                 module.exports = server;
